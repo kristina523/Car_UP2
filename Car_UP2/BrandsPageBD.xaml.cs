d2694dc1fcbae5cfda8caf50e72217ff5cae14f7
RedmiBook <RedmiBook@KROLIK>
2024-10-11 23:10:18 +0300

    Добавьте файлы проекта.

---
+++ Car_UP2/BrandsPageBD.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+using Car_UP2.Properties;
+using Car_UP2;
+namespace Car_UP2
+{
+    /// <summary>
+    /// Логика взаимодействия для BrandsPageBD.xaml
+    /// </summary>
+    public partial class BrandsPageBD : Page
+    {
+        private CarDealershipEntities brands = new CarDealershipEntities();
+        public BrandsPageBD()
+        {
+            InitializeComponent();
+            BD_Brands.ItemsSource = brands.Brands.ToList();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            Brands b = new Brands();
+            b.BrandName = text1.Text;
+
+            brands.Brands.Add(b);
+            brands.SaveChanges();
+            BD_Brands.ItemsSource = brands.Brands.ToList();
+        }
+
+        private void edit_Click(object sender, RoutedEventArgs e)
+        {
+            if (BD_Brands.SelectedItem != null)
+            {
+                var selected = BD_Brands.SelectedItem as Brands;
+
+                selected.BrandName = text1.Text;
+
+                brands.SaveChanges();
+                BD_Brands.ItemsSource = brands.Brands.ToList();
+            }
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBrands = BD_Brands.SelectedItem as Brands;
+            if (selectedBrands != null)
+            {
+                brands.Brands.Remove(selectedBrands);
+                brands.SaveChanges();
+                BD_Brands.ItemsSource = brands.Brands.ToList();
+            }
+        }
+
+        private void BD_Brands_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (BD_Brands.SelectedItem != null)
+            {
+                var selected = BD_Brands.SelectedItem as Brands;
+
+                text1.Text = selected.BrandName;
+
+                brands.SaveChanges();
+                BD_Brands.ItemsSource = brands.Brands.ToList();
+            }
+        }
+    }
+}
---
+++ Car_UP2/Model1.Context.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Car_UP2
+{
+    /// <summary>
+    /// Логика взаимодействия для Model1.xaml
+    /// </summary>
+    
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class CarDealershipEntities : DbContext
+    {
+        public CarDealershipEntities()
+            : base("name=DealershipEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<Brands> Brands { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Models> Models { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Vehicles> Vehicles { get; set; }
+    }
+}
